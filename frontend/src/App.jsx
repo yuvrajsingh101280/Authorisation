9e42cb80aa822be79f0f2f71d4a75a0745992c6a
yuvrajsingh101280 <singhyuvraj65035@gmail.com>
2024-12-19 10:10:35 +0530

    ergerg

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <div className="text-red-500">hello world</div>;
+  return (
+    <div className="min-h-screen bg-gradient-to-r from-emerald-500 to-emerald-900  flex items-center justify-center overflow-hidden relative">
+
+    </div>
+  );
 };
 
 export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router}>
+      {" "}
+      <App />
+    </RouterProvider>
+  </StrictMode>
+);
