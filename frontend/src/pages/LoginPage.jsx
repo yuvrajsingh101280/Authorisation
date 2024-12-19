e6425d2c922d209906030737fdbe7ab65cc7a763
yuvrajsingh101280 <singhyuvraj65035@gmail.com>
2024-12-19 11:48:27 +0530

    complete the login page

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,7 +1,78 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Lock, Mail } from "lucide-react";
+import { motion } from "framer-motion";
+import Input from "../components/input";
+import { Link } from "react-router-dom";
 const LoginPage = () => {
-  return <div>Losgin page</div>;
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      className="max-w-lg  w-[25em] bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden"
+    >
+      <div className="p-8">
+        <h2 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-500 bg-clip-text text-transparent">
+          Welcome Back{" "}
+        </h2>
+
+        <form onSubmit={handleSubmit}>
+          <Input
+            icon={Mail}
+            type="text"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            icon={Lock}
+            type="text"
+            placeholder="Enter your Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          {/* forgot password */}
+
+          <div className="flex items-center mb-6">
+            <Link
+              to="/forgot-password"
+              className="text-sm text-green-200 hover:underline"
+            >
+              Forgot password?
+            </Link>
+          </div>
+          {/* button */}
+          <motion.button
+            className="mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white 
+						font-bold rounded-lg shadow-lg hover:from-green-600
+						hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2
+						 focus:ring-offset-gray-900 transition duration-200"
+            whileHover={{ scale: 1.02 }}
+            whileTap={{ scale: 0.98 }}
+            type="submit"
+          >
+            Login
+          </motion.button>
+        </form>
+      </div>
+
+      <div className="px-8 py-4 bg-gray-900 bg-opacity-50 flex justify-center">
+        <p className="text-sm flex gap-2 text-gray-400">
+          Don't have account?
+          <Link to={"/signup"} className="text-green-400">
+            Sign up
+          </Link>
+        </p>
+      </div>
+    </motion.div>
+  );
 };
 
 export default LoginPage;
