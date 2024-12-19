8d88d2cda5cb2d5923aaf87738d9e09b445d82a6
yuvrajsingh101280 <singhyuvraj65035@gmail.com>
2024-12-19 10:21:51 +0530

    completed the route

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,21 @@ import "./index.css";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import SignupPage from "./pages/SignupPage.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage />,
   },
 ]);
 
