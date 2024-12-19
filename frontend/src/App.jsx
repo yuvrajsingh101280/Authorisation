4f9648b5d8805072c39c4b463227e74eb4be7b41
yuvrajsingh101280 <singhyuvraj65035@gmail.com>
2024-12-19 10:18:23 +0530

    given css to body

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div className="min-h-screen bg-gradient-to-r from-emerald-500 to-emerald-900  flex items-center justify-center overflow-hidden relative">
-
+      jhh
     </div>
   );
 };
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -15,7 +15,6 @@ const router = createBrowserRouter([
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router}>
-      {" "}
       <App />
     </RouterProvider>
   </StrictMode>
